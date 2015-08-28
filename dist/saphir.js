be67fd2b0c994644688066aa3cf1a7e9dd759af4
Tamer Aydin <tamer.aydin@gmail.com>
2015-08-28 13:08:13 +0200

    bump to version 0.2.1

--- dist/saphir.js
+++ dist/saphir.js
@@ -1,5 +1,5 @@
 /**
- * saphir v0.2.0 (https://github.com/tameraydin/saphir)
+ * saphir v0.2.1 (https://github.com/tameraydin/saphir)
  * Copyright 2015 Tamer Aydin (http://tamerayd.in)
  * Licensed under MIT
  */
@@ -7,7 +7,7 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
@@ -189,6 +189,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       }
     }
 
+    // MODULE
+
     _createClass(SaphirObject, [{
       key: '_subscribe',
       value: function _subscribe(prop, callback) {
@@ -210,7 +212,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     return SaphirObject;
   })();
 
-  // MODULE
   var saphir = {
     SaphirObject: SaphirObject,
     SaphirArray: SaphirArray
